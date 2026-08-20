71ae3befb49bda8107546330ee56872b1f24a265
skem-skem <lazenda001@gmail.com>
2026-08-20 15:32:17 +0200

    added og-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,7 +56,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.jpg",
+        url: "/e-book2.webp",
         width: 1200,
         height: 630,
         alt: "The Buried Reality by Jonas Riven",
