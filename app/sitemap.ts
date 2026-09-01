bf7827fc5cc6420c4525de59d02d84367ce42252
skem-skem <lazenda001@gmail.com>
2026-09-01 14:44:05 +0200

    feat: update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -11,7 +11,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: `${baseUrl}/products/the-buried-reality`,
+      url: `${baseUrl}/products/reality-without-witnesses`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
